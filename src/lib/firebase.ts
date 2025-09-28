69e48d612baef111022e6c6ec6a284820104c7ee
Valentina14142000 <124637319+Valentina14142000@users.noreply.github.com>
2025-09-29 00:18:51 +0300

    Add Firebase configuration files and update .gitignore

---
+++ src/lib/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "esncash.firebaseapp.com",
+  projectId: "esncash",
+  storageBucket: "esncash.appspot.com",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
